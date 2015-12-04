a2ecbf5ad3e54493978f35622bb67320767aed6d
CDanilo25 <cdanilo25@gmail.com>
2015-12-04 22:29:19 +0100

    Primordial Version: base created!
    More functions coming soon, just stay tuned!

---
+++ Md5Checker.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-04T21:32:07
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Md5Checker
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,66 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QCryptographicHash>
+#include <QFile>
+
+/*
+ * A primordial version of the program.
+ * More functions coming soon, just stay tuned.
+ * Please.
+ */
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_fileSelectBrowse_clicked()
+{
+    // Open a File Browse dialog and, if a file was selected, the button to start the check become available.
+
+    QString stringFileName = QFileDialog::getOpenFileName(this, tr("Apri"), "", tr("Tutti (*.*)"));
+    if (stringFileName.isNull() || stringFileName.isEmpty())
+    { }
+    else
+    {
+        ui->startCheck->setEnabled(true);
+        ui->fileSelectLocation->setText(stringFileName);
+    }
+}
+
+void MainWindow::on_startCheck_clicked()
+{
+
+    // TODO: better code and more functions.
+
+    // Select file, initialize the byteload and start processing the file, disabling the main window for security purpose.
+    // Reached the ending of file, the checksum is showed to user.
+
+    QString fileName = ui->fileSelectLocation->text();
+    QFile fileSelected(fileName);
+
+    int byteLoad = 262144;
+
+    QCryptographicHash checkProcess(QCryptographicHash::Md5);
+
+        fileSelected.open(QFile::ReadOnly);
+        while(!fileSelected.atEnd())
+        {
+            checkProcess.addData(fileSelected.read(byteLoad));
+            ui->checkInfo->setText(tr("Analisi dell'impronta del file selezionato in corso..."));
+            this->setEnabled(false);
+        }
+
+        this->setEnabled(true);
+        QByteArray md5Data = checkProcess.result();
+
+        ui->checkInfo->setText(tr("MD5 del File scelto: ") + md5Data.toHex() );
+}
